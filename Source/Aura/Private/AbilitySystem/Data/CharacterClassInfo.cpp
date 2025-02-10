ac66fd60b7c0a228fb5ed07f67ad8b0c758535b5
Jean-Philippe Bureau <jeanphilippe.bureau1@gmail.com>
2025-02-10 14:53:44 -0500

    Added CharacterClassInfo for enemy attributes and added json/csv files for their attributes

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(const ECharacterClass CharacterClass) const
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
